e38f04a629b857105d06a97ccf61a825d7282301
Lior Frenkel <lior.frenkel@melio.com>
2023-10-17 14:00:33 +0200

    Remove console

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-  setTimeout(() => {
-    console.log(process.env.TUNNEL_SUBDOMAIN);
-  }
-  , 1000);
   return {
     server: {
       host: 'localhost',
