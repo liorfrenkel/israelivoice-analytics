73c5f0a1d5e149b87c03b21837d51795633c333d
Lior Frenkel <lior.frenkel@melio.com>
2023-10-17 14:00:51 +0200

    Add example query

---
+++ src/monday.client.ts
@@ -0,0 +1,29 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.monday.com/v2/',
+  headers: {
+    Authorization: import.meta.env.VITE_MONDAY_API_KEY || '',
+    'API-Version': '2023-07',
+    'Content-Type': 'application/json',
+  },
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        boards(ids: [1287143776]) {
+          id
+          name
+
+          activity_logs(column_ids: ["status0"], from: "2023-10-07", limit: 100) {
+            data
+            created_at
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
